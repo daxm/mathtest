24dc6880a224b4857854b616ffaabd1931050ee9
Dax MIckelson <dmickels@cisco.com>
2019-01-31 19:46:46 -0700

    building 1 to 120 page

--- js/build-1-to-120-test.js
+++ js/build-1-to-120-test.js
@@ -1,19 +1,19 @@
 function build1To120Test() {
-    var numberList = [];
-    var i = 1;
+    let numberList = [];
+    let i = 1;
     for (i; i <= 120; i++) {
         numberList.push(i);
     }
 
     while (numberList.length > 0) {
         if (numberList.length > 1) {
-            var aRandomPosition = Math.floor(Math.random() * numberList.length) + 1;
+            let aRandomPosition = Math.floor(Math.random() * numberList.length) + 1;
         }
         else {
             aRandomPosition = 0;
         }
-        var aNumber = numberList.splice(aRandomPosition,1);
-        console.log(numberList);
+        let aNumber = numberList.splice(aRandomPosition,1);
+        console.log(aNumber);
     }
 }
 
