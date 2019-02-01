eac1291b002fa1bb442034270db4f0789db73573
Dax MIckelson <dmickels@cisco.com>
2019-01-31 19:32:48 -0700

    building 1 to 120 page

--- default.js
+++ default.js
@@ -5,7 +5,12 @@ for (i; i <= 120; i++) {
 }
 
 while (numberList.length > 0) {
+    if (numberList.length > 1) {
     var aRandomPosition = Math.floor(Math.random() * numberList.length) + 1;
+    }
+    else {
+        aRandomPosition = 0;
+    }
     var aNumber = numberList.splice(aRandomPosition,1);
     console.log(numberList);
 }
