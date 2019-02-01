7406aa71b257a7dd04062d4da3f7339269449189
Dax MIckelson <dmickels@cisco.com>
2019-02-01 03:41:17 -0700

    building 1 to 120 page

--- js/build-1-to-120-test.js
+++ js/build-1-to-120-test.js
@@ -47,9 +47,6 @@ function htmlListMathProblems(theArray) {
         theText = theArray[0] + " + " + theArray[1];
     }
     theText +=  " = ____";
-    while ((15 - theText.length) > 0) {
-        theText += '\xa0';
-    }
     let htmlText = document.createTextNode(theText);
     let div = document.createElement( "div" );
     div.classList.add('math-problem');
